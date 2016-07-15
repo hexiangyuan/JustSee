1a818811ed8275fc8b5b9dcf6a5ca8fa869cd218
何祥源 <hexiangyuan@ezbuy.com>
2016-07-15 17:57:06 +0800

    navigation

--- app/src/main/java/com/just/see/justsee/JustSeeHomeActivity.java
+++ app/src/main/java/com/just/see/justsee/JustSeeHomeActivity.java
@@ -35,6 +35,13 @@ public class JustSeeHomeActivity extends AppCompatActivity
             }
         });
 
+        initDrawerLayout(toolbar);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        DaXiangFragment daXiangFragment = new DaXiangFragment();
+        supportFragmentManager.beginTransaction().add(R.id.container, daXiangFragment).commit();
+    }
+
+    private void initDrawerLayout(Toolbar toolbar) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -43,9 +50,6 @@ public class JustSeeHomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        FragmentManager supportFragmentManager = getSupportFragmentManager();
-        DaXiangFragment daXiangFragment = new DaXiangFragment();
-        supportFragmentManager.beginTransaction().add(R.id.container, daXiangFragment).commit();
     }
 
     @Override
@@ -60,23 +64,11 @@ public class JustSeeHomeActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.just_see_home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
