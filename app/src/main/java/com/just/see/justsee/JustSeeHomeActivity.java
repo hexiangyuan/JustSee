5a96f35add0bf73075c2f9129f71d24991f53eb4
何祥源 <hexiangyuan@ezbuy.com>
2016-07-19 11:09:32 +0800

    横竖屏切

--- app/src/main/java/com/just/see/justsee/JustSeeHomeActivity.java
+++ app/src/main/java/com/just/see/justsee/JustSeeHomeActivity.java
@@ -1,11 +1,11 @@
 package com.just.see.justsee;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,8 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.just.see.justsee.daxiang.ui.DaXiangFragment;
+import com.orhanobut.logger.Logger;
 
 public class JustSeeHomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +24,7 @@ public class JustSeeHomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logger.d("onCreate");
         setContentView(R.layout.activity_just_see_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -95,5 +98,11 @@ public class JustSeeHomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Logger.d("onConfigurationChanged");
     }
 }
