023622e315e9154d3c65bd634c8ac1391f075f3f
何祥源 <hexiangyuan@ezbuy.com>
2016-07-27 17:06:26 +0800

    react-natice model

--- app/src/main/java/com/just/see/justsee/reactnative/JustSeeReactActivity.java
+++ app/src/main/java/com/just/see/justsee/reactnative/JustSeeReactActivity.java
@@ -1,17 +1,85 @@
 package com.just.see.justsee.reactnative;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+import com.just.see.justsee.BuildConfig;
 
 /**
  * Created by 何祥源 on 16/7/27.
  * Desc:
  */
 public class JustSeeReactActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView reactRootView;
+    private ReactInstanceManager reactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        reactRootView = new ReactRootView(this);
+        reactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        reactRootView.startReactApplication(reactInstanceManager, "HelloWorld", null);
+        setContentView(reactRootView);
+    }
 
     @Override
     public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onPause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onResume(this, this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_MENU && reactInstanceManager != null){
+            reactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
