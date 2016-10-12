5ad0748e866987ff8ccca9374fd27f08c3e81c77
何祥源 <hexiangyuan@ezbuy.com>
2016-10-12 15:22:54 +0800

    add splash

--- app/src/main/java/com/just/see/justsee/SplashActivity.java
+++ app/src/main/java/com/just/see/justsee/SplashActivity.java
@@ -1,6 +1,11 @@
 package com.just.see.justsee;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
 
 /**
  * Created by 何祥源 on 16/10/12.
@@ -8,5 +13,16 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class SplashActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, JustSeeHomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1500);
+    }
 }
